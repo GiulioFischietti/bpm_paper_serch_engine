9f4a86c7dfeee4cb1a7f4f61629bd7d85270668f
dgl1797 <digiacomoluca1797@gmail.com>
2023-06-29 21:51:53 +0200

    /summary endpoint

--- server/endpoints.py
+++ server/endpoints.py
@@ -9,6 +9,7 @@ from schemes import ErrorScheme, SearchResponse
 from access_points import qdrand_client, mongo_articles, transformer
 from config import OPENAI_API_KEY, OPENAI_FREE_URL, ALLOWED_ORIGINS
 from openai import ChatCompletion
+import json
 import requests
 
 
@@ -28,21 +29,45 @@ def get_search(query: str, page: int):
     }
   })
 
+  final_response = [SearchResponse(
+    doc["title"], 
+    doc["publication_date"], 
+    doc["summary"], 
+    doc["link"], 
+    doc["authors"]
+  ) for doc in parsings if "summary" in doc]
+
+  return JSONResponse(jsonable_encoder(ErrorScheme("frek't", HTTPStatus.BAD_REQUEST))) if query == ""\
+    else JSONResponse(jsonable_encoder(final_response))
+
+@app.put("/summary")
+def get_summary(links:list[str]):
+  parsings = mongo_articles.find({
+    "link":{
+      "$in": links
+    }
+  })
+
+  # TODO: request to openAI for the response
   '''
-    prompt = "You have been asked: " + query +  " by a phd. Provide a  800-1000 word answer with introduction, discussion and conclusions considering the following pieces of articles found on sciencedirect, considering that some of them may belong to the same article or be of the same topic and mentioning them if necessary like this: [0], [1] and so on:\n" 
+    prompt = "You have been asked: " + query +  " by a phd. Provide a  800-1000 word answer with title, introduction, discussion and conclusions considering the following pieces of articles found on sciencedirect, considering that some of them may belong to the same article or be of the same topic and mentioning them if necessary like this: [0], [1] and so on:\n" 
     for idx, doc in enumerate(parsings):
       prompt += f'{idx}. title: "{doc["title"]}" {doc["abstract"]}\n'
       continue
   '''
   
-  # TODO: request to openAI for the response
+  section_text = ""
+  emptyStack = True
+  for section in ["introduction", "discussion", "conclusions"]:
+    section_text = f"<h2>{section.upper()}<h2>"
+    section_text+="<p>"
+    for idx, doc in enumerate(parsings):
+      emptyStack = False
+      if section == "introduction" and idx == 0:
+        section_text = f'<h1>{doc["title"]}</h1>'
+      section_text += doc["abstract"][:30] if section == "introduction" else\
+        doc["body"][:100] if section == "discussion" else doc["conclusions"][:30]
+      section_text += f'<a href={doc["link"]}>[{idx}]</a>'
+    section_text+="</p>"
 
-  final_response = [SearchResponse(doc["title"], doc["publication_date"], doc["abstract"], doc["link"], doc["authors"]) for doc in parsings]
-
-  return JSONResponse(jsonable_encoder(ErrorScheme("frek't", HTTPStatus.BAD_REQUEST))) if query == ""\
-    else JSONResponse(jsonable_encoder(final_response))
-
-@app.get("/summary")
-def get_summary(links:list[str]):
-  print("Hellooo")
-  return JSONResponse(jsonable_encoder(ErrorScheme("frek't", HTTPStatus.BAD_REQUEST)))
+  return section_text if not emptyStack else jsonable_encoder(ErrorScheme("frek't", HTTPStatus.BAD_REQUEST))
